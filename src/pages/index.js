72e8684fe00d852ca8f94f0a67d05de4dd08745c
Sam <samuel_morgan_@outlook.com>
2020-05-30 19:33:24 +0100

    Change title size & position

--- src/pages/index.js
+++ src/pages/index.js
@@ -29,10 +29,9 @@ export default function Home() {
 
   const titleStyle = {
     position: "absolute",
-    top: "50%",
-    left: "50%",
-    transform: "translate(-50%, -100%)",
+    top: "40%",
     color: "white",
+    fontSize: "2rem",
   }
 
   return (
