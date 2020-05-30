e6abeba50f5a391825699973e2be08c8db86deb0
Sam <samuel_morgan_@outlook.com>
2020-05-30 18:59:25 +0100

    Basic title/home styles

--- src/pages/index.js
+++ src/pages/index.js
@@ -1,5 +1,26 @@
 import React from "react"
 
 export default function Home() {
-  return <div>Hello world!</div>
+  const homeStyle = {
+    position: "absolute",
+    top: "0",
+    left: "0",
+    height: "100vh",
+    width: "100vw",
+    display: "flex",
+    alignItems: "center",
+    flexDirection: "column",
+  }
+
+  const titleStyle = {
+    margin: "6rem",
+  }
+
+  return (
+    <div style={homeStyle}>
+      <h1 style={titleStyle}>
+        Sam Morgan<br></br>UK/USA Based Web Developer
+      </h1>
+    </div>
+  )
 }
