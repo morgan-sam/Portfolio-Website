72196134cedb08913a1701c9a8b9fd23b45d355a
Sam <samuel_morgan_@outlook.com>
2020-06-03 21:40:20 +0100

    Basic projects image layout

--- src/components/ThirdSection.jsx
+++ src/components/ThirdSection.jsx
@@ -1,4 +1,6 @@
 import React from 'react';
+import traintracks from '../img/traintracks.jpeg';
+import platform from '../img/platform.jpeg';
 
 const thirdSectionStyle = {
 	position: 'relative',
@@ -14,14 +16,46 @@ const thirdSectionStyle = {
 const titleStyle = {
 	fontSize: '2rem',
 	textDecorationLine: 'underline',
-	top: '10%',
+	top: '12%',
 	position: 'absolute'
+};
+
+const subHeadingStyle = {
+	textAlign: 'center',
+	fontSize: '2rem',
+	textDecorationLine: 'underline',
+	margin: '3rem'
+};
+
+const imgStyles = {
+	height: '25rem',
+	border: '1px solid #444'
+};
+
+const imgContainerStyle = {
+	display: 'flex'
+};
+
+const subContainerStyle = {
+	display: 'flex',
+	flexDirection: 'column',
+	margin: '1rem'
 };
 
 const ThirdSection = () => {
 	return (
 		<div style={thirdSectionStyle}>
 			<div style={titleStyle}>Projects</div>
+			<div style={imgContainerStyle}>
+				<div style={subContainerStyle}>
+					<div style={subHeadingStyle}>Train Tracks Game</div>
+					<img style={imgStyles} src={traintracks} alt="traintracks" />
+				</div>
+				<div style={subContainerStyle}>
+					<div style={subHeadingStyle}>Project Management Platform</div>
+					<img style={imgStyles} src={platform} alt="platform" />
+				</div>
+			</div>
 		</div>
 	);
 };
