2aa40b9ec71e81caba2b837fc783bafd74692588
Sam <samuel_morgan_@outlook.com>
2020-06-03 20:00:27 +0100

    Added also

--- src/pages/index.js
+++ src/pages/index.js
@@ -78,7 +78,7 @@ export default function Home() {
 						text={[
 							'Using:',
 							'HTML, CSS, JavaScript, React, PostgreSQL, Webpack, Jest, Linux',
-							'Used:',
+							'Also used:',
 							'MongoDB, Python, Bash'
 						]}
 					/>
