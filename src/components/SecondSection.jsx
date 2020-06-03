afd5a0a4f507660f6581c61d30c19e0dd0cfb73d
Sam <samuel_morgan_@outlook.com>
2020-06-03 21:09:29 +0100

    Second section comp

---
+++ src/components/SecondSection.jsx
@@ -0,0 +1,63 @@
+import React from 'react';
+import Card from '../components/Card';
+import { middleSectionStyle } from '../styles/homePage';
+
+const cardGapPercentage = 25;
+
+const cardStyle = {
+	width: '17rem',
+	height: '17rem',
+	position: ' absolute'
+};
+
+const SecondSection = () => {
+	return (
+		<div style={middleSectionStyle}>
+			<div style={{ position: 'absolute', height: '50%', width: '5px', backgroundColor: '#444' }} />
+			<div style={{ position: 'relative', display: 'block', margin: '0 auto', width: '50rem', height: '75%' }}>
+				<Card
+					style={{
+						...cardStyle,
+						top: '0',
+						left: `${50 + cardGapPercentage}%`,
+						transform: 'translateX(-50%)'
+					}}
+					title={'Bio'}
+					text={[
+						'I am a front end developer who loves programming and solving problems.',
+						'I like to build software from scratch using minimal libraries.'
+					]}
+				/>
+				<Card
+					style={{
+						...cardStyle,
+						top: '50%',
+						left: `${50 - cardGapPercentage}%`,
+						transform: 'translate(-50%,-50%)'
+					}}
+					title={'Skills'}
+					text={
+						'Lorem ipsum dolor sit amet, consectetur adipiscing elit. Etiam semper neque quis libero vehicula molestie. Nulla facilisi. Nunc sollicitudin mauris ac lorem vestibulum sagittis.'
+					}
+				/>
+				<Card
+					style={{
+						...cardStyle,
+						bottom: '0',
+						left: `${50 + cardGapPercentage}%`,
+						transform: 'translateX(-50%)'
+					}}
+					title={'Tech'}
+					text={[
+						'Using:',
+						'HTML, CSS, JavaScript, React, PostgreSQL, Webpack, Jest, Linux',
+						'Also used:',
+						'Python, MongoDB, Bash'
+					]}
+				/>
+			</div>
+		</div>
+	);
+};
+
+export default SecondSection;
--- src/pages/index.js
+++ src/pages/index.js
@@ -1,20 +1,12 @@
 import React, { useState, useEffect } from 'react';
 import TopNavBar from '../components/TopNavBar';
-import Card from '../components/Card';
 import FirstSection from '../components/FirstSection';
-import { middleSectionStyle, homeStyle } from '../styles/homePage';
+import SecondSection from '../components/SecondSection';
+import { homeStyle } from '../styles/homePage';
 
 export default function Home() {
 	const [ scrollPosition, setSrollPosition ] = useState(0);
 	const [ navColor, setNavColor ] = useState(0);
-
-	const cardGapPercentage = 25;
-
-	const cardStyle = {
-		width: '17rem',
-		height: '17rem',
-		position: ' absolute'
-	};
 
 	useEffect(() => {
 		window.addEventListener('scroll', () => setSrollPosition(window.pageYOffset));
@@ -34,53 +26,7 @@ export default function Home() {
 		<div style={homeStyle}>
 			<TopNavBar color={navColor} />
 			<FirstSection />
-			<div style={middleSectionStyle}>
-				<div style={{ position: 'absolute', height: '50%', width: '5px', backgroundColor: '#444' }} />
-				<div
-					style={{ position: 'relative', display: 'block', margin: '0 auto', width: '50rem', height: '75%' }}
-				>
-					<Card
-						style={{
-							...cardStyle,
-							top: '0',
-							left: `${50 + cardGapPercentage}%`,
-							transform: 'translateX(-50%)'
-						}}
-						title={'Bio'}
-						text={[
-							'I am a front end developer who loves programming and solving problems.',
-							'I like to build software from scratch using minimal libraries.'
-						]}
-					/>
-					<Card
-						style={{
-							...cardStyle,
-							top: '50%',
-							left: `${50 - cardGapPercentage}%`,
-							transform: 'translate(-50%,-50%)'
-						}}
-						title={'Skills'}
-						text={
-							'Lorem ipsum dolor sit amet, consectetur adipiscing elit. Etiam semper neque quis libero vehicula molestie. Nulla facilisi. Nunc sollicitudin mauris ac lorem vestibulum sagittis.'
-						}
-					/>
-					<Card
-						style={{
-							...cardStyle,
-							bottom: '0',
-							left: `${50 + cardGapPercentage}%`,
-							transform: 'translateX(-50%)'
-						}}
-						title={'Tech'}
-						text={[
-							'Using:',
-							'HTML, CSS, JavaScript, React, PostgreSQL, Webpack, Jest, Linux',
-							'Also used:',
-							'Python, MongoDB, Bash'
-						]}
-					/>
-				</div>
-			</div>
+			<SecondSection />
 		</div>
 	);
 }
