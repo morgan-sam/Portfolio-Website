34e7cb9bc9f3727b28dbdbbc26df36652d649f8f
Sam <samuel_morgan_@outlook.com>
2020-06-03 18:12:48 +0100

    Text on card fades out

--- src/components/Card.jsx
+++ src/components/Card.jsx
@@ -8,21 +8,45 @@ const Card = (props) => {
 		border: '5px solid #444',
 		borderRadius: '10%',
 		boxShadow: '6px 10px #888',
-		backgroundColor: '#fff'
+		backgroundColor: '#fff',
+		overflow: 'hidden'
 	};
 
 	const titleStyle = {
-		position: 'absolute',
-		top: '10%',
-		left: '50%',
-		transform: 'translateX(-50%)',
+		margin: '1rem auto',
+		display: 'block',
+		textAlign: 'center',
 		fontSize: '2rem',
 		textDecorationLine: 'underline'
+	};
+
+	const textStyle = {
+		display: 'block',
+		margin: '1.5rem auto',
+		width: '80%',
+		fontSize: '1.2rem',
+		boxSizing: 'border-box',
+		overflow: 'hidden'
+	};
+
+	const fadeOverlayStyle = {
+		position: 'absolute',
+		top: '0',
+		left: '0',
+		height: '100%',
+		width: '100%',
+		borderRadius: '10%',
+		background: 'linear-gradient(to bottom, rgba(255, 255, 255, 0) 80%, rgba(255, 255, 255, 1) 100%)'
 	};
 
 	return (
 		<div style={cardStyle}>
 			<div style={titleStyle}>Hello</div>
+			<div style={textStyle}>
+				Lorem ipsum dolor sit, amet consectetur adipisicing elit. Eum aperiam facere suscipit eveniet nisi
+				blanditiis consectetur!
+				<div style={fadeOverlayStyle} />
+			</div>
 		</div>
 	);
 };
