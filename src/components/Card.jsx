39f1bd15b3fa5cd9f928bfc52553263d6b523f51
Sam <samuel_morgan_@outlook.com>
2020-06-03 16:15:32 +0100

    Slight edit to px

--- src/components/Card.jsx
+++ src/components/Card.jsx
@@ -6,7 +6,7 @@ const Card = (props) => {
 		width: '14%',
 		border: '5px solid #444',
 		borderRadius: '10%',
-		boxShadow: '8px 10px #888'
+		boxShadow: '6px 10px #888'
 	};
 
 	return <div style={cardStyle}> </div>;
