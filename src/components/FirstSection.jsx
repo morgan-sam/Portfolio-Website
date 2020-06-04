9f192243c56f742167a4ab86c03c31335489b5bd
Sam <samuel_morgan_@outlook.com>
2020-06-04 16:30:25 +0100

    Replaced firstSection with splash.css

--- src/components/FirstSection.jsx
+++ src/components/FirstSection.jsx
@@ -1,13 +1,12 @@
 import React from 'react';
-import { firstSectionStyle, backgroundStyle, titleStyle } from '../styles/firstSection';
 
 const FirstSection = React.forwardRef((props, ref) => {
 	return (
-		<div className={'section'} style={firstSectionStyle} ref={ref}>
-			<div style={titleStyle}>
+		<div className={'section'} ref={ref}>
+			<div className={'splashTitle'}>
 				Sam Morgan<br />UK/USA Front End Developer
 			</div>
-			<div style={backgroundStyle} />
+			<div className={'splashBackground'} />
 		</div>
 	);
 });
