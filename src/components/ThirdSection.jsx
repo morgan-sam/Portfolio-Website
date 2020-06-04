b725c63001374048c1e3f74c6595772f2afda9bb
Sam <samuel_morgan_@outlook.com>
2020-06-04 10:33:08 +0100

    thirdSection styles sep file

--- src/components/ThirdSection.jsx
+++ src/components/ThirdSection.jsx
@@ -2,52 +2,15 @@ import React from 'react';
 import traintracks from '../img/traintracks.jpeg';
 import platform from '../img/platform.jpeg';
 
-const thirdSectionStyle = {
-	position: 'relative',
-	display: 'block',
-	height: '100vh',
-	width: '100%',
-	display: 'flex',
-	alignItems: 'center',
-	justifyContent: 'center',
-	flexDirection: 'column',
-	backgroundColor: '#fff'
-};
-
-const titleStyle = {
-	fontSize: '2rem',
-	textDecorationLine: 'underline',
-	top: '12%',
-	position: 'absolute'
-};
-
-const subHeadingStyle = {
-	textAlign: 'center',
-	fontSize: '2rem',
-	textDecorationLine: 'underline',
-	margin: '3rem'
-};
-
-const imgStyles = {
-	height: '25rem',
-	border: '1px solid #444'
-};
-
-const imgContainerStyle = {
-	display: 'flex'
-};
-
-const subContainerStyle = {
-	display: 'flex',
-	flexDirection: 'column',
-	margin: '1rem'
-};
-
-const linkTextStyle = {
-	textAlign: 'center',
-	fontSize: '2rem',
-	margin: '3rem'
-};
+import {
+	thirdSectionStyle,
+	titleStyle,
+	subHeadingStyle,
+	imgStyles,
+	imgContainerStyle,
+	subContainerStyle,
+	linkTextStyle
+} from '../styles/thirdSection';
 
 const ThirdSection = React.forwardRef((props, ref) => {
 	return (
---
+++ src/styles/thirdSection.js
@@ -0,0 +1,46 @@
+export const thirdSectionStyle = {
+	position: 'relative',
+	display: 'block',
+	height: '100vh',
+	width: '100%',
+	display: 'flex',
+	alignItems: 'center',
+	justifyContent: 'center',
+	flexDirection: 'column',
+	backgroundColor: '#fff'
+};
+
+export const titleStyle = {
+	fontSize: '2rem',
+	textDecorationLine: 'underline',
+	top: '12%',
+	position: 'absolute'
+};
+
+export const subHeadingStyle = {
+	textAlign: 'center',
+	fontSize: '2rem',
+	textDecorationLine: 'underline',
+	margin: '3rem'
+};
+
+export const imgStyles = {
+	height: '25rem',
+	border: '1px solid #444'
+};
+
+export const imgContainerStyle = {
+	display: 'flex'
+};
+
+export const subContainerStyle = {
+	display: 'flex',
+	flexDirection: 'column',
+	margin: '1rem'
+};
+
+export const linkTextStyle = {
+	textAlign: 'center',
+	fontSize: '2rem',
+	margin: '3rem'
+};
