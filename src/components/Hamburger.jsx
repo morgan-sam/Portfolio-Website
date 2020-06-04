b75e34da7ae0d0a0fb3adab9a0f4e67a0da1bb7d
Sam <samuel_morgan_@outlook.com>
2020-06-04 20:38:49 +0100

    Hamburger opens button names

--- src/components/Hamburger.jsx
+++ src/components/Hamburger.jsx
@@ -3,11 +3,16 @@ import React, { useState } from 'react';
 const Hamburger = (props) => {
 	const [ open, setOpen ] = useState(false);
 
+	const buttons = [ 'home', 'about', 'projects', 'contact' ];
+
 	return (
-		<div className={'hamburger'} onClick={() => setOpen(!open)}>
-			<div className={`hamburgerLine ${open ? 'openHamburgerLine' : null}`} />
-			<div className={`hamburgerLine ${open ? 'openHamburgerLine' : null}`} />
-			<div className={`hamburgerLine ${open ? 'openHamburgerLine' : null}`} />
+		<div className={'hamburger'}>
+			<div className={'hamburgerButton'} onClick={() => setOpen(!open)}>
+				<div className={`hamburgerLine ${open ? 'openHamburgerLine' : null}`} />
+				<div className={`hamburgerLine ${open ? 'openHamburgerLine' : null}`} />
+				<div className={`hamburgerLine ${open ? 'openHamburgerLine' : null}`} />
+			</div>
+			{open && <div className={'hamburgerDropdown'}>{buttons.map((el) => <div>{el}</div>)}</div>}
 		</div>
 	);
 };
