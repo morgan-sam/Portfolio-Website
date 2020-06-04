2e475f5755d2bc7546d03a81f5fd702bd10bc416
Sam <samuel_morgan_@outlook.com>
2020-06-04 10:35:05 +0100

    fourthSectionStyle styles sep file

--- src/components/FourthSection.jsx
+++ src/components/FourthSection.jsx
@@ -1,38 +1,6 @@
 import React from 'react';
 
-const imageURL =
-	'https://images.unsplash.com/photo-1448838520990-b0f959a7da46?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=1489&q=80';
-
-const fourthSectionStyle = {
-	position: 'relative',
-	display: 'block',
-	height: '100vh',
-	width: '100%',
-	display: 'flex',
-	alignItems: 'center',
-	justifyContent: 'center',
-	flexDirection: 'column',
-	backgroundColor: '#efeeff'
-};
-
-const contactContainerStyle = {
-	position: 'absolute',
-	height: '20rem',
-	width: '20rem',
-	backgroundColor: '#efeeff',
-	display: 'flex',
-	alignItems: 'center',
-	justifyContent: 'center',
-	flexDirection: 'column',
-	borderRadius: '100%'
-};
-
-const contactTextStyle = {
-	fontSize: '1.3rem',
-	margin: '0.35rem',
-	textAlign: 'center',
-	color: '#444'
-};
+import { imageURL, fourthSectionStyle, contactContainerStyle, contactTextStyle } from '../styles/fourthSection';
 
 const FourthSection = React.forwardRef((props, ref) => {
 	return (
---
+++ src/styles/fourthSection.js
@@ -0,0 +1,33 @@
+export const imageURL =
+	'https://images.unsplash.com/photo-1448838520990-b0f959a7da46?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=1489&q=80';
+
+export const fourthSectionStyle = {
+	position: 'relative',
+	display: 'block',
+	height: '100vh',
+	width: '100%',
+	display: 'flex',
+	alignItems: 'center',
+	justifyContent: 'center',
+	flexDirection: 'column',
+	backgroundColor: '#efeeff'
+};
+
+export const contactContainerStyle = {
+	position: 'absolute',
+	height: '20rem',
+	width: '20rem',
+	backgroundColor: '#efeeff',
+	display: 'flex',
+	alignItems: 'center',
+	justifyContent: 'center',
+	flexDirection: 'column',
+	borderRadius: '100%'
+};
+
+export const contactTextStyle = {
+	fontSize: '1.3rem',
+	margin: '0.35rem',
+	textAlign: 'center',
+	color: '#444'
+};
