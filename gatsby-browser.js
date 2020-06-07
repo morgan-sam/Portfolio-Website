6d7245ab87bf80b363b2f8ff94b5fba73935e5af
Sam <samuel_morgan_@outlook.com>
2020-06-07 19:53:47 +0100

    Reordered css import order, fix nav btn bug

--- gatsby-browser.js
+++ gatsby-browser.js
@@ -6,7 +6,8 @@ import './src/styles/splash.css';
 import './src/styles/about.css';
 import './src/styles/projects.css';
 import './src/styles/contact.css';
-import './src/styles/queries.css';
 
 import './src/styles/buttons.css';
 import './src/styles/card.css';
+
+import './src/styles/queries.css';
