a430ad547482c7e604b80c6569690d0ae340912a
Sam <samuel_morgan_@outlook.com>
2020-09-01 22:29:18 +0100

    Adding outlook email

--- src/components/Contact.jsx
+++ src/components/Contact.jsx
@@ -1,26 +1,26 @@
-import React from 'react';
+import React from "react"
 
 const Contact = React.forwardRef((props, ref) => {
-	return (
-		<div className={'section contactSection'} ref={ref}>
-			<div className={'contactImage'} />
-			<div className={'contactContainer'}>
-				<div className={'contactText'}>Contact</div>
-				<div className={'contactText'}>hello@smorgan.dev</div>
-				<div className={'contactText'}>
-					<a href="https://github.com/morgan-sam">
-						<div className="linkText">https://github.com/morgan-sam</div>
-						<div className="linkImg" />
-					</a>
-				</div>
-				<div className={'contactText'}>
-					{/* <a href="smorgan.dev/cv">Curriculum Vitae</a> */}
-					<div>THANK YOU</div>
-				</div>
-				<div className={'contactText'}>© 2020</div>
-			</div>
-		</div>
-	);
-});
+  return (
+    <div className={"section contactSection"} ref={ref}>
+      <div className={"contactImage"} />
+      <div className={"contactContainer"}>
+        <div className={"contactText"}>Contact</div>
+        <div className={"contactText"}>samuel_morgan_@outlook.com</div>
+        <div className={"contactText"}>
+          <a href="https://github.com/morgan-sam">
+            <div className="linkText">https://github.com/morgan-sam</div>
+            <div className="linkImg" />
+          </a>
+        </div>
+        <div className={"contactText"}>
+          {/* <a href="smorgan.dev/cv">Curriculum Vitae</a> */}
+          <div>THANK YOU</div>
+        </div>
+        <div className={"contactText"}>© 2020</div>
+      </div>
+    </div>
+  )
+})
 
-export default Contact;
+export default Contact
