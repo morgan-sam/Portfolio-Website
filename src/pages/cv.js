6735232f388bf13fce1ca08cd330e4ddfaf5930e
Sam <samuel_morgan_@outlook.com>
2020-10-02 14:13:12 +0100

    CV Loading text large and centered

--- src/pages/cv.js
+++ src/pages/cv.js
@@ -1,7 +1,8 @@
 import React, { useEffect } from "react"
 import { CV_LINK } from "../data/file_links"
+import "../styles/cv_loading.css"
 
 export default () => {
   useEffect(() => (window.location = CV_LINK), [])
-  return <div>Loading CV...</div>
+  return <div className="cv-loading">Loading CV...</div>
 }
