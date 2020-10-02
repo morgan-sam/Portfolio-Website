d9ff81e9f1cf04cb847a5fe57cde34d31bb1fb9d
Sam <samuel_morgan_@outlook.com>
2020-10-02 14:10:21 +0100

    CV_LINK centralised

--- src/components/Contact.jsx
+++ src/components/Contact.jsx
@@ -1,7 +1,5 @@
 import React from "react"
-
-const CV_LINK =
-  "https://firebasestorage.googleapis.com/v0/b/portfolio-website-f9e63.appspot.com/o/cv.pdf?alt=media&token=0a70f3d9-f111-4441-9437-3c400d28873b"
+import { CV_LINK } from "../data/file_links"
 
 const Contact = React.forwardRef((props, ref) => {
   return (
---
+++ src/data/file_links.js
@@ -0,0 +1,2 @@
+export const CV_LINK =
+  "https://firebasestorage.googleapis.com/v0/b/portfolio-website-f9e63.appspot.com/o/cv.pdf?alt=media&token=0a70f3d9-f111-4441-9437-3c400d28873b"
