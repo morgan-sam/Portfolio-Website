843b35579fa51c20102c809bb6ba2e96207d7310
Sam <samuel_morgan_@outlook.com>
2020-10-02 14:06:05 +0100

    Fixed with useEffect

--- src/pages/cv.js
+++ src/pages/cv.js
@@ -1,5 +1,8 @@
-import React from "react"
+import React, { useEffect } from "react"
+const CV =
+  "https://firebasestorage.googleapis.com/v0/b/portfolio-website-f9e63.appspot.com/o/cv.pdf?alt=media&token=0a70f3d9-f111-4441-9437-3c400d28873b"
 
-export default () =>
-  (window.location =
-    "https://firebasestorage.googleapis.com/v0/b/portfolio-website-f9e63.appspot.com/o/cv.pdf?alt=media&token=0a70f3d9-f111-4441-9437-3c400d28873b")
+export default () => {
+  useEffect(() => (window.location = CV), [])
+  return <div>Loading CV...</div>
+}
