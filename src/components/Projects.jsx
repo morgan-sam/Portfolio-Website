ffa2a8df887a1a4838accb8212de35276e017564
Sam <samuel_morgan_@outlook.com>
2020-11-11 21:04:03 +0000

    Replaced pomodoro project with dealsmate

--- src/components/Projects.jsx
+++ src/components/Projects.jsx
@@ -1,48 +1,49 @@
-import React, { useState, useEffect } from 'react';
-import OnVisible from './OnVisible';
-import ProjectCard from './ProjectCard';
+import React, { useState, useEffect } from "react"
+import OnVisible from "./OnVisible"
+import ProjectCard from "./ProjectCard"
 
 const Projects = React.forwardRef((props, ref) => {
-	const [ focused, setFocused ] = useState(null);
-	useEffect(() => setFocused(null), [ props.scrollPosition ]);
-	const cardCommonProps = {
-		focused,
-		setFocused,
-		mobileView: typeof window !== 'undefined' && window.matchMedia('(max-width: 600px)').matches
-	};
+  const [focused, setFocused] = useState(null)
+  useEffect(() => setFocused(null), [props.scrollPosition])
+  const cardCommonProps = {
+    focused,
+    setFocused,
+    mobileView:
+      typeof window !== "undefined" &&
+      window.matchMedia("(max-width: 600px)").matches,
+  }
 
-	return (
-		<div className={'section projectSection'} ref={ref}>
-			<OnVisible effect={'fade-in'}>
-				<div className={'projectTitle'}>Projects</div>
-			</OnVisible>
-			<div className={'projectContainer'}>
-				<ProjectCard
-					fadeDelay={0}
-					className={'platform'}
-					title={'Project Management Platform'}
-					source={'https://github.com/morgan-sam/Project-Management-Platform'}
-					{...cardCommonProps}
-				/>
-				<ProjectCard
-					fadeDelay={200}
-					className={'traintracks'}
-					title={'Train Tracks Game'}
-					live={'https://morgan-sam.github.io/Train-Tracks-React/'}
-					source={'https://github.com/morgan-sam/Train-Tracks-React'}
-					{...cardCommonProps}
-				/>
-				<ProjectCard
-					fadeDelay={400}
-					className={'pomodoro'}
-					title={'Pomodoro Tracker'}
-					live={'https://pomodorotracker.app/'}
-					source={'https://github.com/morgan-sam/Pomodoro-Tracker'}
-					{...cardCommonProps}
-				/>
-			</div>
-		</div>
-	);
-});
+  return (
+    <div className={"section projectSection"} ref={ref}>
+      <OnVisible effect={"fade-in"}>
+        <div className={"projectTitle"}>Projects</div>
+      </OnVisible>
+      <div className={"projectContainer"}>
+        <ProjectCard
+          fadeDelay={0}
+          className={"dealsmate"}
+          title={"Dealsmate"}
+          source={"https://github.com/hatchways/team-apple-cider"}
+          {...cardCommonProps}
+        />
+        <ProjectCard
+          fadeDelay={200}
+          className={"platform"}
+          title={"Project Management Platform"}
+          source={"https://github.com/morgan-sam/Project-Management-Platform"}
+          {...cardCommonProps}
+        />
+        <ProjectCard
+          fadeDelay={400}
+          className={"traintracks"}
+          title={"Train Tracks Game"}
+          live={"https://morgan-sam.github.io/Train-Tracks-React/"}
+          source={"https://github.com/morgan-sam/Train-Tracks-React"}
+          {...cardCommonProps}
+        />
+      </div>
+    </div>
+  )
+})
 
-export default Projects;
+export default Projects
